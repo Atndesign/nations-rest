1c2059d943565677ba9b81c71bfdc4f7ba4c9f91
Atndesign <Atnnetworkk@gmail.com>
2021-04-19 15:36:09 +0200

    Add the searchbar

--- src/components/homepage/index.vue
+++ src/components/homepage/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="homepage">
     <Header />
+    <Input />
   </div>
 </template>
 
 <script>
 import Header from "../header";
+import Input from "../homepage/input";
 
 export default {
   name: "HomePage",
   components: {
     Header,
+    Input,
   },
 };
 </script>
---
+++ src/components/homepage/input.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="searchbar">
+    <div class="searchbar__container">
+      <img src="../../assets/img/search.svg" alt="" srcset="" />
+      <input
+        type="text"
+        class="searchbar-container__input"
+        placeholder="Search for a country"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+};
+</script>
+
+<style></style>
