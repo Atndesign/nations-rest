a4a7682429a6d83ea0e30f1309b369dd79cc510d
Atndesign <Atnnetworkk@gmail.com>
2021-04-19 19:42:08 +0200

    Add the drop down opening function

--- src/components/homepage/dropdown.vue
+++ src/components/homepage/dropdown.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="dropdown">
-    <div class="dropdown__select">
+    <div class="dropdown__select" @click="openDropDown">
       Filter by region
     </div>
-    <!-- <ul class="dropdown__list">
+    <ul
+      class="dropdown__list"
+      :style="{ display: isOpen === true ? 'block' : 'none' }"
+    >
       <li class="list__item">Africa</li>
       <li class="list__item">America</li>
       <li class="list__item">Asia</li>
       <li class="list__item">Europe</li>
-    </ul> -->
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "DropDown",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    openDropDown() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
 
