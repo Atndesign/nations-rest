1f2d30c832b6bbca01bdefdfb36ee53a565e345a
Atndesign <Atnnetworkk@gmail.com>
2021-04-19 15:08:44 +0200

    Add the dark btn on the header

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,7 +2,9 @@
   <header class="header">
     <p class="header__text">Where in the world</p>
     <div class="header__dark-mode">
-      <div class="dark-mode__btn"></div>
+      <div class="dark-mode__btn">
+        <img src="../assets/img/dark.svg" alt="" srcset="" />
+      </div>
       <div class="dark-mode__text">Dark mode</div>
     </div>
   </header>
