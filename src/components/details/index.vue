73f2503457f3b5cc7555c2302a8c6b025412cc23
Atndesign <Atnnetworkk@gmail.com>
2021-04-20 13:07:01 +0200

    Add border countries styel

--- src/components/details/index.vue
+++ src/components/details/index.vue
@@ -4,7 +4,99 @@
     <div class="top-controls">
       <BackBtn @close-detail="closeDetailView" />
     </div>
-    <div class="infos"></div>
+
+    <div class="infos" v-if="countryInfos?.flag">
+      <img
+        class="details__flag"
+        v-if="countryInfos?.flag"
+        :src="countryInfos.flag"
+      />
+      <div class="infos__content">
+        <p class="content__country-name">{{ countryInfos.name }}</p>
+        <p class="content__name">
+          Native name:
+          <span class="content__value">
+            {{ countryInfos.nativeName }}
+          </span>
+        </p>
+        <p class="content__name">
+          Population:
+          <span class="content__value">
+            {{
+              countryInfos.population
+                .toString()
+                .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+            }}
+          </span>
+        </p>
+        <p class="content__name">
+          Region:
+          <span class="content__value">
+            {{ countryInfos.region }}
+          </span>
+        </p>
+        <p class="content__name">
+          Sub region:
+          <span class="content__value">
+            {{ countryInfos.subregion }}
+          </span>
+        </p>
+        <p class="content__name">
+          Capital:
+          <span class="content__value">
+            {{ countryInfos.capital }}
+          </span>
+        </p>
+      </div>
+      <div class="infos__content">
+        <p class="content__name">
+          Top level domain:
+          <span class="content__value">
+            {{
+              countryInfos.topLevelDomain
+                .map((domain) => {
+                  return domain;
+                })
+                .toString()
+            }}
+          </span>
+        </p>
+        <p class="content__name">
+          Currencies:
+          <span class="content__value">
+            {{
+              countryInfos.currencies
+                .map((currency) => currency.name + " ")
+                .toString()
+            }}
+          </span>
+        </p>
+        <p class="content__name" v-if="countryInfos?.languages">
+          Languages:
+          <span class="content__value">
+            {{
+              countryInfos.languages
+                .map((language) => language.name + " ")
+                .toString()
+            }}
+          </span>
+        </p>
+      </div>
+      <div class="content__borders">
+        <p class="borders__title" v-if="countryInfos?.languages">
+          Border countries:
+        </p>
+        <div class="borders__items">
+          <button
+            class="borders__item"
+            v-for="(border, index) in countryInfos.borders"
+            :key="index"
+          >
+            {{ border }}
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,13 +113,12 @@ export default {
   },
   data() {
     return {
-      countryInfos: null,
+      countryInfos: {},
     };
   },
   mounted() {
     axios.get("https://restcountries.eu/rest/v2/name/belgium").then((data) => {
-      this.countryInfos = data.data;
-      console.log(data.data[0]);
+      this.countryInfos = data.data[0];
     });
   },
   methods: {
