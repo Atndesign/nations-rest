91aaddaa469ed1d1b030fe98d562473f7584d476
Atndesign <Atnnetworkk@gmail.com>
2021-04-20 13:39:21 +0200

    Add responsive to the detial page

--- src/components/details/index.vue
+++ src/components/details/index.vue
@@ -11,90 +11,94 @@
         v-if="countryInfos?.flag"
         :src="countryInfos.flag"
       />
-      <div class="infos__content">
+      <div class="infos__main">
         <p class="content__country-name">{{ countryInfos.name }}</p>
-        <p class="content__name">
-          Native name:
-          <span class="content__value">
-            {{ countryInfos.nativeName }}
-          </span>
-        </p>
-        <p class="content__name">
-          Population:
-          <span class="content__value">
-            {{
-              countryInfos.population
-                .toString()
-                .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
-            }}
-          </span>
-        </p>
-        <p class="content__name">
-          Region:
-          <span class="content__value">
-            {{ countryInfos.region }}
-          </span>
-        </p>
-        <p class="content__name">
-          Sub region:
-          <span class="content__value">
-            {{ countryInfos.subregion }}
-          </span>
-        </p>
-        <p class="content__name">
-          Capital:
-          <span class="content__value">
-            {{ countryInfos.capital }}
-          </span>
-        </p>
-      </div>
-      <div class="infos__content">
-        <p class="content__name">
-          Top level domain:
-          <span class="content__value">
-            {{
-              countryInfos.topLevelDomain
-                .map((domain) => {
-                  return domain;
-                })
-                .toString()
-            }}
-          </span>
-        </p>
-        <p class="content__name">
-          Currencies:
-          <span class="content__value">
-            {{
-              countryInfos.currencies
-                .map((currency) => currency.name + " ")
-                .toString()
-            }}
-          </span>
-        </p>
-        <p class="content__name" v-if="countryInfos?.languages">
-          Languages:
-          <span class="content__value">
-            {{
-              countryInfos.languages
-                .map((language) => language.name + " ")
-                .toString()
-            }}
-          </span>
-        </p>
-      </div>
-      <div class="content__borders">
-        <p class="borders__title" v-if="countryInfos?.languages">
-          Border countries:
-        </p>
-        <div class="borders__items">
-          <button
-            class="borders__item"
-            v-for="(border, index) in countryInfos.borders"
-            :key="index"
-            @click="fecthCountry(border)"
-          >
-            {{ border }}
-          </button>
+        <div class="main__infos">
+          <div class="infos__content">
+            <p class="content__name">
+              Native name:
+              <span class="content__value">
+                {{ countryInfos.nativeName }}
+              </span>
+            </p>
+            <p class="content__name">
+              Population:
+              <span class="content__value">
+                {{
+                  countryInfos.population
+                    .toString()
+                    .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+                }}
+              </span>
+            </p>
+            <p class="content__name">
+              Region:
+              <span class="content__value">
+                {{ countryInfos.region }}
+              </span>
+            </p>
+            <p class="content__name">
+              Sub region:
+              <span class="content__value">
+                {{ countryInfos.subregion }}
+              </span>
+            </p>
+            <p class="content__name">
+              Capital:
+              <span class="content__value">
+                {{ countryInfos.capital }}
+              </span>
+            </p>
+          </div>
+          <div class="infos__content">
+            <p class="content__name">
+              Top level domain:
+              <span class="content__value">
+                {{
+                  countryInfos.topLevelDomain
+                    .map((domain) => {
+                      return domain;
+                    })
+                    .toString()
+                }}
+              </span>
+            </p>
+            <p class="content__name">
+              Currencies:
+              <span class="content__value">
+                {{
+                  countryInfos.currencies
+                    .map((currency) => currency.name + " ")
+                    .toString()
+                }}
+              </span>
+            </p>
+            <p class="content__name" v-if="countryInfos?.languages">
+              Languages:
+              <span class="content__value">
+                {{
+                  countryInfos.languages
+                    .map((language) => language.name + " ")
+                    .toString()
+                }}
+              </span>
+            </p>
+          </div>
+        </div>
+        <div class="content__borders">
+          <p class="borders__title" v-if="countryInfos?.languages">
+            Border countries:
+          </p>
+          <div class="borders__items">
+            <button
+              class="borders__item"
+              v-for="(border, index) in countryInfos.borders"
+              :key="index"
+              @click="fecthCountry(border)"
+            >
+              {{ border }}
+            </button>
+          </div>
         </div>
       </div>
     </div>
